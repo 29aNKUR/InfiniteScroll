415b6d21a6142976742ed003a5cf18df652aaf5f
Ankur Semle <semleankur@gmail.com>
2024-02-17 00:52:24 +0530

    done

--- src/components/InfiniteScroll.jsx
+++ src/components/InfiniteScroll.jsx
@@ -3,20 +3,36 @@ import View from './View';
 
 const InfiniteScroll = () => {
     const [response, setResponse] = useState([]);
-    const [page, setPage] = useState(0);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
 
     const getData = async () => {
         const res = await fetch(`https://jsonplaceholder.typicode.com/posts?` + new URLSearchParams({_limit: 9, _page: page}) )
         const data = await res.json();
         // console.log(data);
-        setResponse(data);
+        setResponse((oldData) => [...oldData, ...data]);
+        setLoading(false);
     }
     useEffect(() => {
         getData();
-    })
+    },[page])
+
+    const handleInfiniteScroll = async () => {
+        if(window.innerHeight + document.documentElement.scrollTop >= document.documentElement.scrollHeight){
+            // alert(1);
+            setLoading(true);
+            setPage((prev) => prev + 1);
+        }
+      
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleInfiniteScroll)
+    },[]);
   return (
     <div>
         <View response = {response}/>
+        {loading && <div className='loading'></div> }
     </div>
   )
 }
